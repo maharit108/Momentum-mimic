e2eb968d5203b24328f6450efb1787534ac2e4a9
maharit108 <maharit108@gmail.com>
2020-10-04 23:14:18 -0500

    Updates

---
+++ precache-manifest.ee4cb8e7f9028fd7d68f2a29f82ba13d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7188061b5fa9e0992442a48fef1b3f64",
+    "url": "/Momentum-mimic/index.html"
+  },
+  {
+    "revision": "84b11633397688570de4",
+    "url": "/Momentum-mimic/static/css/main.0ec29612.chunk.css"
+  },
+  {
+    "revision": "30ffc793abc1d3804bae",
+    "url": "/Momentum-mimic/static/js/2.a113681f.chunk.js"
+  },
+  {
+    "revision": "bbee919d8888a17953b5bb98359759ba",
+    "url": "/Momentum-mimic/static/js/2.a113681f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84b11633397688570de4",
+    "url": "/Momentum-mimic/static/js/main.86524591.chunk.js"
+  },
+  {
+    "revision": "217c2aa96ecd9665631f",
+    "url": "/Momentum-mimic/static/js/runtime-main.7451168e.js"
+  }
+]);
